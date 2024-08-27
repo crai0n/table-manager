ab7c2e783c92baab8eb49fcb037315e439d25a3d
crai0n <52631259+crai0n@users.noreply.github.com>
2024-08-27 13:16:06 +0200

    added utoipa for api documentation
    3 API documentations: Swagger, Redoc and Scalar

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, get, HttpResponse, HttpServer, post, Responder, web};
+use utoipa::OpenApi;
+use utoipa_redoc::{Redoc, Servable};
+use utoipa_scalar::{Scalar, Servable as ScalarServable};
+use utoipa_swagger_ui::SwaggerUi;
 
+#[utoipa::path(
+    responses(
+        (status = 200, description = "Successful response", body = String)
+    ),
+    tag = "table"
+)]
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -16,11 +26,24 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    #[derive(OpenApi)]
+    #[openapi(paths(hello))]
+    struct ApiDoc;
+
+    let openapi = ApiDoc::openapi();
+
+
+    HttpServer::new(move || {
         App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .service(SwaggerUi::new("/swagger-ui/{_:.*}").url(
+                "/api-docs/openapi.json",
+                openapi.clone(),
+            ))
+            .service(Redoc::with_url("/redoc", openapi.clone()))
+            .service(Scalar::with_url("/scalar", openapi.clone()))
     })
         .bind(("127.0.0.1", 8080))?
         .run()
