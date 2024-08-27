a3c0fad335237e4e5279a5a11a73b4f5dc38c457
crai0n <52631259+crai0n@users.noreply.github.com>
2024-08-27 18:15:28 +0200

    used utoipa chrono feature

--- src/models/table.rs
+++ src/models/table.rs
@@ -5,9 +5,7 @@ use utoipa::ToSchema;
 #[derive(Debug, Serialize, Deserialize, ToSchema, Clone)]
 pub struct Table {
     pub id: Option<String>,
-    #[schema(value_type = Option < String >)]
     pub created_at: Option<DateTime<Utc>>,
-    #[schema(value_type = Option < String >)]
     pub updated_at: Option<DateTime<Utc>>,
     pub name: String,
     pub owner: Option<String>,
